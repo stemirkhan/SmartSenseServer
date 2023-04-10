e766e4ff89dcc63fa8c25ce9a7b2c803a243a478
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-04-10 23:20:54 +0300

    added validation of arguments coming to url

--- handler.py
+++ handler.py
@@ -1,25 +1,22 @@
 from http.server import BaseHTTPRequestHandler
-from utils import parse_request_client
-from settings import ACCESS_TOKEN
+from utils import parse_request_client, validator_url_arguments
+from settings import *
 from db import WorkerDB
 
-db = WorkerDB('postgres', 'qwe', 'localhost', '5432', 'sensor_data')
+db = WorkerDB(USER_DB, PASSWORD_DB, HOST_DB, PORT_DB, DATABASE)
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         arguments = parse_request_client(self.path)
-
-        if arguments['access_token'] == ACCESS_TOKEN:
-            db.add_measurements(arguments['temperature'],
-                                arguments['pressure'],
-                                arguments['carbonMonoxide'],
-                                arguments['humidity'])
+        if validator_url_arguments(arguments) and arguments['access_token'] == ACCESS_TOKEN:
+            db.add_measurements(arguments['temperature'], arguments['pressure'],
+                                arguments['carbonMonoxide'], arguments['humidity'])
             self.send_response(200)
 
         else:
-            self.send_response(401)
+            self.send_error(401)
 
         self.end_headers()
         self.wfile.write(b'')
