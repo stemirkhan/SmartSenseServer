0a56c67f2f10aa46ac91e87db3fdd3e01a7711e3
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-04-10 23:23:14 +0300

    moved database connection settings here

--- settings.py
+++ settings.py
@@ -10,9 +10,19 @@ if DEBUG:
     HOST = '192.168.0.100'
     ACCESS_TOKEN = 'f7f0391dc01b4261fa09d4c26cbda146081c4820ceffb0e4c453c0a41a057ceccec' \
                    '074cd2423246d206d018ed9a9396db100df5b7bc32616151217893eab1fc4'
+    USER_DB = 'postgres'
+    HOST_DB = 'localhost'
+    PORT_DB = '5432'
+    PASSWORD_DB = 'qwe'
+    DATABASE = 'sensor_data'
 
 
 else:
     ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
     PORT = 53344
     HOST = '0.0.0.0'
+    USER_DB = ''
+    HOST_DB = ''
+    PORT_DB = ''
+    PASSWORD_DB = os.getenv("PASSWORD_DB")
+    DATABASE = ''
