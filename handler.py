bcb304a54fdb254915332454e665de73361145a9
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-04-16 12:40:09 +0300

    Changes related to logging and fault tolerance

--- handler.py
+++ handler.py
@@ -2,8 +2,10 @@ from http.server import BaseHTTPRequestHandler
 from utils import parse_request_client, validator_url_arguments
 from settings import *
 from db import WorkerDB
+from log import server_logger
 
 db = WorkerDB(USER_DB, PASSWORD_DB, HOST_DB, PORT_DB, DATABASE)
+db.create_table()
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
@@ -13,10 +15,12 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         if validator_url_arguments(arguments) and arguments['access_token'] == ACCESS_TOKEN:
             db.add_measurements(arguments['temperature'], arguments['pressure'],
                                 arguments['carbonMonoxide'], arguments['humidity'])
-            self.send_response(200)
+            self.send_response(200, 'OK')
 
         else:
-            self.send_error(401)
+            self.log_request(401, "invalid access_token")
 
         self.end_headers()
-        self.wfile.write(b'')
+
+    def log_message(self, format_msg: str, *args) -> None:
+        server_logger.info("%s - - [%s] %s\n" % (self.address_string(), self.log_date_time_string(), format_msg%args))
--- run.py
+++ run.py
@@ -2,12 +2,14 @@ from http.server import HTTPServer
 from handler import HTTPRequestHandler
 from settings import *
 from ssl import SSLContext, PROTOCOL_TLS_SERVER
-
+from log import server_logger
 
 if __name__ == '__main__':
-    httpd = HTTPServer((HOST, PORT), HTTPRequestHandler)
-    sslctx = SSLContext(PROTOCOL_TLS_SERVER)
-    sslctx.load_cert_chain(certfile=PATH_CERTIFICATE, keyfile=PATH_PRIVATE_KEY)
-    httpd.socket = sslctx.wrap_socket(httpd.socket, server_side=True)
-    httpd.serve_forever()
-
+    try:
+        httpd = HTTPServer((HOST, PORT), HTTPRequestHandler)
+        sslctx = SSLContext(PROTOCOL_TLS_SERVER)
+        sslctx.load_cert_chain(certfile=PATH_CERTIFICATE, keyfile=PATH_PRIVATE_KEY)
+        httpd.socket = sslctx.wrap_socket(httpd.socket, server_side=True)
+        httpd.serve_forever()
+    except Exception:
+        server_logger.exception("Error server startup")
