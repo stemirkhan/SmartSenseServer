35dc62b2810f370d44b1637f87d38b345c005893
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-05-10 21:21:34 +0300

    Added the function to unload tokens from the database

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,6 @@
 from psycopg2 import connect
 from log import db_logger
+from settings import ACCESS_TOKEN_TABLE, SENSOR_READINGS_TABLE
 
 
 class WorkerDB:
@@ -16,12 +17,12 @@ class WorkerDB:
             db_logger.exception("Error connecting to database")
 
     def create_table(self):
-        sql_create_table_command = """CREATE TABLE IF NOT EXISTS sensor_readings(
-                                      id serial NOT NULL primary key,
-                                      recording_time TIMESTAMP NOT NULL DEFAULT now(),
+        sql_create_table_command = f"""CREATE TABLE IF NOT EXISTS {SENSOR_READINGS_TABLE}(
+                                      id serial primary key,
+                                      time TIMESTAMP DEFAULT now(),
                                       temperature double precision,
                                       pressure double precision,
-                                      carbon_monoxide double precision,
+                                      carbon_monoxide integer,
                                       humidity double precision);"""
 
         try:
@@ -34,8 +35,8 @@ class WorkerDB:
                          pressure: str,
                          carbon_monoxide: str,
                          humidity: str):
-        sql_insert_command = """
-                            INSERT INTO sensor_readings (
+        sql_insert_command = f"""
+                            INSERT INTO {SENSOR_READINGS_TABLE} (
                             temperature, 
                             pressure,
                             carbon_monoxide,
@@ -48,6 +49,17 @@ class WorkerDB:
             self.connection_db.commit()
         except Exception:
             db_logger.exception(f"Error adding record: {temperature}, {pressure}, {carbon_monoxide}, {humidity}")
+
+    def get_access_tokens(self):
+        access_tokens = []
+        try:
+            sql_get_tokens_command = f"""SELECT * FROM {ACCESS_TOKEN_TABLE};"""
+            self._cursor.execute(sql_get_tokens_command)
+            access_tokens = self._cursor.fetchall()
+        except Exception:
+            db_logger.exception("Error get access tokes")
+
+        return [token[-1] for token in access_tokens]
 
     def __del__(self):
         self.connection_db.close()
